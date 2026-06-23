bc19b85b97cd147cbd7d66f941bc31b933d3a5e6
James Cogo <127156142+jamescogo@users.noreply.github.com>
2026-06-22 20:36:49 -0700

    Create SetupSSH.sh

---
+++ SetupSSH.sh
@@ -0,0 +1,20 @@
+#Create your SSH keys
+ssh-keygen -t ed25519 -C "SuperSecretHax-OrWhateverYouWant_LikeEmail_Or_Something"
+# Answer the prompts, but if you're super lazy, just hit enter twice and not save a passcode for your ssh key, allowing anyone with access to it to steal and use it.
+
+#Secure your keys
+#Sets read-write-execute for the owner only.
+chmod 700 ~/.ssh
+#Sets read-write for the owner only
+chmod 600 ~/.ssh/id_ed25519
+
+#If on windows, copy and paste the private key below into a text file.
+cat ~/.ssh/id_ed25519
+
+#From here, open putty-gen and load the key. 
+#You will then be prompted that you imported a key and to save the private key (*.ppk file) to use it with putty.
+#Do that. Save the private key locally where you can find your .ppk file.
+#Open up puTTY and navigate on the side bar to Connection > SSH > Auth > Credentials 
+#Then in the private key for authentication, load your .ppk file with the browse button
+#Before opening, navigate back to the session category, click on Default Settings or create a different setting and click on SAVE.
+#If you dont do that, you'll lose your configuration and wont be able to lazily ssh into your computer.
